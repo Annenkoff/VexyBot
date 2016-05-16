321d5af9548ce6cd61cd1c70770012c088ee2401
ZZooRM <annkoff@ya.ru>
2016-05-16 22:00:05 +0300

    Added operation Delete

--- src/main/java/vexybot/dao/NotesManager.java
+++ src/main/java/vexybot/dao/NotesManager.java
@@ -1,13 +1,10 @@
 package vexybot.dao;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import vexybot.entity.Note;
 import vexybot.util.HibernateSessionFactory;
 
 public class NotesManager {
-    private SessionFactory sessionFactory;
-
     public static void add(int chatId, String name, String text) {
         Session session = HibernateSessionFactory.getSessionFactory().openSession();
         try {
@@ -19,6 +16,18 @@ public class NotesManager {
             session.save(note);
             session.getTransaction().commit();
         } catch (Exception e) {
+        }
+    }
+
+    public static void delete(int id) {
+        Session session = HibernateSessionFactory.getSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            Note note = (Note) session.get(Note.class, id);
+            session.delete(note);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+
         }
     }
 }
