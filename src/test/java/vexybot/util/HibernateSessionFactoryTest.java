685d93dbf322d98abd501f73174c60e7674e9824
ZZooRM <ZZooRM@ya.ru>
2016-06-18 01:31:34 +0300

    Added JUnit tests.

---
+++ src/test/java/vexybot/util/HibernateSessionFactoryTest.java
@@ -0,0 +1,15 @@
+package vexybot.util;
+
+import org.junit.Test;
+
+public class HibernateSessionFactoryTest {
+    @Test
+    public void getSessionFactory() throws Exception {
+
+    }
+
+    @Test
+    public void shutdown() throws Exception {
+
+    }
+}
