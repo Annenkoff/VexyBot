ac15caea90f1fbeed500bd85214785b0518bf86e
Annenkoff <contact@annenkov.me>
2016-08-24 01:38:22 +0300

    Added comments in Config class.

--- src/main/java/su/vexy/vexybot/Config.java
+++ src/main/java/su/vexy/vexybot/Config.java
@@ -1,5 +1,8 @@
 package su.vexy.vexybot;
 
+/**
+ * Содержит основные конфигурации.
+ */
 public class Config {
     public static final String USERNAME = "vexybot"; // Username of the bot.
     public static final String TOKEN = "180193131:AAGGhh0lBoU2fn1-bzS3RGkjmiYAip2N9MU"; //The wrong token ;)
