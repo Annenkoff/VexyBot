e6d1261345dcaaaed959ee77551523bd25482f70
Annenkoff <contact@annenkov.me>
2016-08-24 01:32:10 +0300

    Added comments in Config class.

--- src/main/java/su/vexy/vexybot/Config.java
+++ src/main/java/su/vexy/vexybot/Config.java
@@ -1,8 +1,8 @@
 package su.vexy.vexybot;
 
 public class Config {
-    public static final String USERNAME = "vexybot";
+    public static final String USERNAME = "vexybot"; // Username of the bot.
     public static final String TOKEN = "180193131:AAGGhh0lBoU2fn1-bzS3RGkjmiYAip2N9MU"; //The wrong token ;)
-    public static final String RESOURCE_PATH = "locale.";
+    public static final String RESOURCE_PATH = "locale."; //Path with locales.
     public static final String ANSWERS = "messages";
 }
