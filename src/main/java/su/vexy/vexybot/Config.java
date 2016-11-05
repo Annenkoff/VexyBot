7a90241063cd410c8cc50be77edde121c1a0a807
Annenkoff <zzoorm@ya.ru>
2016-11-05 15:15:43 +0300

    Changes.

--- src/main/java/su/vexy/vexybot/Config.java
+++ src/main/java/su/vexy/vexybot/Config.java
@@ -5,6 +5,6 @@ package su.vexy.vexybot;
  */
 public class Config {
     public static final String USERNAME = "vexybot"; // Username of the bot.
-    public static final String TOKEN = "180193131:AAGGhh0lBoU2fn1-bzS3RGkjmiYAip2N9MU"; //The wrong token ;)
+    public static final String TOKEN = "204690429:AAHjXVT5z4KnVUAiss8Kc7gNSfaUtgKYUO4"; //The wrong token ;)
     public static final String RESOURCE_PATH = "locale."; //Path with locales.
 }
