4058568782eed1d4425d1201aa1956242fd5bc71
Annenkoff <zzoorm@ya.ru>
2016-11-05 15:35:07 +0300

    Changes.

--- src/main/java/su/vexy/vexybot/Main.java
+++ src/main/java/su/vexy/vexybot/Main.java
@@ -5,11 +5,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.telegram.telegrambots.TelegramApiException;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import su.vexy.vexybot.services.HibernateSessionFactory;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        HibernateSessionFactory.getSessionFactory().openSession();
         ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
         TelegramBotsApi telegramBotsApi = (TelegramBotsApi) context.getBean("telegramBotsApi"); // Create the telegram bot object.
         try {
