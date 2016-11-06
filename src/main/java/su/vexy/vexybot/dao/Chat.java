b7aff8ddeabbc6f26cd7ea38097f21ab54633dca
Annenkoff <zzoorm@ya.ru>
2016-11-06 11:37:58 +0300

    Added fn and ln to the class Chat.

--- src/main/java/su/vexy/vexybot/dao/Chat.java
+++ src/main/java/su/vexy/vexybot/dao/Chat.java
@@ -6,11 +6,13 @@ import javax.persistence.*;
 @Table(name = "chats", schema = "a0080939_vexybot", catalog = "")
 public class Chat {
     private int id;
-    private String status;
+    private String firstName;
+    private String lastName;
     private String locale;
     private String location;
-    private int morningNotification;
-    private int eveningNotification;
+    private Integer morningNotification;
+    private Integer eveningNotification;
+    private String status;
 
     @Id
     @Column(name = "id")
@@ -23,13 +25,23 @@ public class Chat {
     }
 
     @Basic
-    @Column(name = "status")
-    public String getStatus() {
-        return status;
+    @Column(name = "first_name")
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Basic
+    @Column(name = "last_name")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     @Basic
@@ -54,22 +66,32 @@ public class Chat {
 
     @Basic
     @Column(name = "morning_notification")
-    public int getMorningNotification() {
+    public Integer getMorningNotification() {
         return morningNotification;
     }
 
-    public void setMorningNotification(int morningNotification) {
+    public void setMorningNotification(Integer morningNotification) {
         this.morningNotification = morningNotification;
     }
 
     @Basic
     @Column(name = "evening_notification")
-    public int getEveningNotification() {
+    public Integer getEveningNotification() {
         return eveningNotification;
     }
 
-    public void setEveningNotification(int eveningNotification) {
+    public void setEveningNotification(Integer eveningNotification) {
         this.eveningNotification = eveningNotification;
+    }
+
+    @Basic
+    @Column(name = "status")
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 
     @Override
@@ -80,11 +102,15 @@ public class Chat {
         Chat that = (Chat) o;
 
         if (id != that.id) return false;
-        if (morningNotification != that.morningNotification) return false;
-        if (eveningNotification != that.eveningNotification) return false;
-        if (status != null ? !status.equals(that.status) : that.status != null) return false;
+        if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(that.lastName) : that.lastName != null) return false;
         if (locale != null ? !locale.equals(that.locale) : that.locale != null) return false;
         if (location != null ? !location.equals(that.location) : that.location != null) return false;
+        if (morningNotification != null ? !morningNotification.equals(that.morningNotification) : that.morningNotification != null)
+            return false;
+        if (eveningNotification != null ? !eveningNotification.equals(that.eveningNotification) : that.eveningNotification != null)
+            return false;
+        if (status != null ? !status.equals(that.status) : that.status != null) return false;
 
         return true;
     }
@@ -92,11 +118,13 @@ public class Chat {
     @Override
     public int hashCode() {
         int result = id;
-        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
         result = 31 * result + (locale != null ? locale.hashCode() : 0);
         result = 31 * result + (location != null ? location.hashCode() : 0);
-        result = 31 * result + morningNotification;
-        result = 31 * result + eveningNotification;
+        result = 31 * result + (morningNotification != null ? morningNotification.hashCode() : 0);
+        result = 31 * result + (eveningNotification != null ? eveningNotification.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
         return result;
     }
 }
